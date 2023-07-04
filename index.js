7deaec7b3288d4b766cb16cc54dbef676ef1910e
Bhagwan73 <bhagwan735025@gmail.com>
2023-07-05 00:09:58 +0530

    cors added

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const route = require("./src/route/route")
 const app = express()
 app.use(express.json())
 require('dotenv').config({path:"./.env"});
+const cors = require('cors');
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();  
-});
+app.use(cors());
 
 app.use("/", route)
 app.listen(process.env.PORT || 3001, () => {
